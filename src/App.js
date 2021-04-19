f4c935b32b3ff9d8646e8cb08c780ef9c3f61f51
Vindymuasa <S21810184@student.com-->
2021-04-19 21:45:17 +0800

    exercise7

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeScreen from './components/pages/HomeScreen';
+import Register from './components/pages/Register'
+
+const App = () => {
+    return (
+        <Register />
+    )
+};
+
+
+export default App;
